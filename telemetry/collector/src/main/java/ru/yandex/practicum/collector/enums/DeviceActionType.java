730c7a0905ced1f364d7e5af50e1520f72156878
madoka <obivanyakenobi@gmail.com>
2026-06-03 05:07:44 +0300

    refactoring: create hub and sensor builder

---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/enums/DeviceActionType.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.collector.enums;
+
+public enum DeviceActionType {
+    ACTIVATE,
+    DEACTIVATE,
+    INVERSE,
+    SET_VALUE
+}
---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/enums/ScenarioConditionOperationType.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.collector.enums;
+
+public enum ScenarioConditionOperationType {
+    EQUALS,
+    GREATER_THAN,
+    LOWER_THAN
+}
